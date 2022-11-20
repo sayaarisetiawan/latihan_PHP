e7110929a4d3aacc0007f203fae928838f4e3bad
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-20 16:45:21 +0700

    perintah tugas 4

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar PHP</title>
-</head>
-<body>
-    <!-- tugas 3 -->
- <h1> SELAMAT DATANG
- <?php echo $nama; ?> </h1>
-</body>
-</html>
+<?php
+$nama = " ari setiawan";
+echo "hallo, selamat datang $nama </br>";
+echo 'hallo, selamat datang $nama';
+?>
